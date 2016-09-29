f84237ef051e60254b06995033ec0424e274465a
Miraz <miraz.freedom@gmail.com>
2016-09-29 17:51:20 +0600

    rework without video-playlist plugin

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -1,39 +1,4 @@
 videojs.options.flash.swf = "../vendors/video.js/video-js.swf";
-var naTpl = (function($) {
-    var jsTemplates = {};
-    var getJsTemplates = function() {
-        var templates = {};
-        $('script[type="text/js-template"]').each(function(key, value) {
-            var id = $(value).attr('id');
-            if (id)
-                templates[id] = $(value).html();
-        });
-        return templates;
-    };
-    var replaceDataInPlaceholder = function(data, id) {
-        var str = '';
-        if (jsTemplates[id]) {
-            str = jsTemplates[id].replace(/\{\{(.*?)\}\}/g, function(match, token) {
-                var value = eval('data.' + token.trim());
-                return value ? value : '';
-            });
-        }
-        //console.log(str);
-        return str;
-    };
-    var getTemplate = function(data, tplId) {
-        var htmlStr = replaceDataInPlaceholder(data, tplId);
-        return htmlStr === '' ? null : $(htmlStr);
-    };
-    var initialize = function() {
-        jsTemplates = getJsTemplates();
-    };
-    return {
-        getTemplate: getTemplate,
-        initialize: initialize
-    }
-})(jQuery);
-/* Custom */
 (function($) {
     'use strict';
 
