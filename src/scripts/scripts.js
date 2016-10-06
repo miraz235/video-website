1d6607758124bfde26e15561d768c223f992216d
Miraz <miraz.freedom@gmail.com>
2016-10-06 15:00:36 +0600

    video playlist modularizing and resource folder added

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -3,8 +3,7 @@ videojs.options.flash.swf = "../vendors/video.js/video-js.swf";
     'use strict';
 
     $(document).ready(function() {
-        var $tracksDom = $('.media-playlist__tracks li .media-playlist__video');
-        var queries = {};
+
         var getUrlQueries = function(queryStr) {
             var out = {};
             $.each(queryStr.split('&'), function(key, value) {
@@ -13,39 +12,64 @@ videojs.options.flash.swf = "../vendors/video.js/video-js.swf";
             });
             return out;
         };
-        queries = getUrlQueries(document.location.search.substr(1));
-        var vplayerId = queries.watch;
-        var videoList = [];
-        var currentIndex = -1;
 
-        $tracksDom.each(function(index) {
-            var urldata = getUrlQueries(this.href.split('?')[1]);
-            if (urldata) {
-                var trackId = urldata.watch;
-                if (trackId === vplayerId) {
-                    $(this).parent().addClass("currently-playing");
-                    currentIndex = index;
+        var videoPlayList = {
+            init: function() {
+                this.currentIndex = -1;
+                this.videoList = [];
+                this.setMediaId();
+                if (this.mediaId) {
+                    this.setVideoList();
+                    this.setVideoPlayer();
                 }
-                videoList[index] = trackId;
-            }
-        });
+            },
+            setMediaId: function() {
+                var queries = getUrlQueries(document.location.search.substr(1));
+                this.mediaId = queries.watch
+            },
+            setVideoList: function() {
+                var self = this;
+                var $tracksDom = $('.media-playlist__tracks li .media-playlist__video');
+                $tracksDom.each(function(index) {
+                    var urldata = getUrlQueries(this.href.split('?')[1]);
+                    var $li = $(this).parent();
+                    if (urldata) {
+                        var trackId = urldata.watch;
+                        self.videoList[index] = trackId;
+                        if (trackId === self.mediaId) {
+                            $li.addClass("currently-playing");
+                            self.currentIndex = index;
+                            $li.parent().animate({
+                                scrollTop: index * $li.outerHeight() + 1
+                            }, 1000);
+                        } else if (self.currentIndex > -1)
+                            return false;
+                    }
+                });
+            },
+            setVideoPlayer: function() {
+                var self = this;
+                var if_autoplay = true;
+                var vplayer = videojs("videoMediaPlayer", { controls: true, autoplay: if_autoplay, preload: "none" }, function() {
+                    if (self.mediaId) {
+                        this.src([{ type: "video/mp4", src: "resources/videos/" + self.mediaId + ".mp4" }]);
+                        this.poster('resources/videos/posters/' + self.mediaId + '.jpg');
+                    }
+                });
+                vplayer.on('ended', $.proxy(this.setVideoEndEvent, this));
+            },
+            setVideoEndEvent: function(e) {
+                var nextVideo = this.videoList[this.currentIndex + 1];
+                if (nextVideo) {
+                    setTimeout(function() {
+                        window.location.href = "video-playlist.html?watch=" + nextVideo;
+                    }, 1500)
 
-        var if_autoplay = true;
-        var vplayer = videojs("videoMediaPlayer", { controls: true, autoplay: if_autoplay, preload: "none" }, function() {
-            if (vplayerId) {
-                this.src([{ type: "video/mp4", src: "videos/" + vplayerId + ".mp4" }]);
-                this.poster('videos/posters/' + vplayerId + '.jpg');
+                }
             }
-        });
-        vplayer.on('ended', function() {
-            console.log('awww...over so soon?');
-            if (videoList[currentIndex + 1]) {
-                setTimeout(function() {
-                    window.location.href = "video-playlist.html?watch=" + videoList[currentIndex + 1];
-                }, 1500)
+        };
 
-            }
-        });
+        videoPlayList.init();
 
     });
 
