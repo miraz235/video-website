291cdb1124da30a847766e3592fcce73736e908f
Miraz <miraz.freedom@gmail.com>
2016-12-22 14:50:19 +0600

    scripts

--- src/scripts/scripts.js
+++ src/scripts/scripts.js
@@ -2,32 +2,7 @@
     'use strict';
 
     $(document).ready(function() {
-        var videoSingle = bloggMedia('video').setMediaPlayer("#videoSingle", {
-            html5: {
-                hlsjsConfig: {
-                    autoStartLoad: true,
-                    debug: true
-                }
-            }
-        });
-        videoSingle.setPlugins({
-            watermark: { image: '' }
-        });
-        var videoMediaPlayer = bloggMedia('video').setMediaPlayer("#videoMediaPlayer", {
-            html5: {
-                hlsjsConfig: {
-                    autoStartLoad: true,
-                    debug: true
-                }
-            }
-        }).setPlayList('.media-playlist__tracks li .media-playlist__video');
-        var audioSingle = bloggMedia('audio').setMediaPlayer("#audioSingle");
 
-        var audiosInPage = bloggMedia('audio');
-        var $audios = $("audio.js-media-audio");
-        $audios.each(function(index) {
-            audiosInPage.addMediaPlayer(this);
-        });
     });
 
 })(jQuery);
